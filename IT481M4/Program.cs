e4580bea946e3cfa58f314b0686fa6e5e710e99e
Kory Jacobs <kory.jacobs@pb.com>
2020-12-14 16:29:46 -0800

    Add project files.

---
+++ IT481M4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace IT481M4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int min = 0;
+            int max = 10000;
+            int[] small = new int[10];
+            int[] medium = new int[100];
+            int[] large = new int[1000];
+
+            // load the arrays with random numbers. 
+            Random randomSmall = new Random();
+            for (int i = 0; i < small.Length; i++)
+            {
+                small[i] = randomSmall.Next(min, max);
+            }
+
+            Random randomMedium = new Random();
+            for (int i = 0; i < medium.Length; i++)
+            {
+                medium[i] = randomMedium.Next(min, max);
+            }
+
+            Random randomLarge = new Random();
+            for (int i = 0; i < large.Length; i++)
+            {
+                large[i] = randomLarge.Next(min, max);
+            }
+
+
+            for (int i = 0; i < small.Length; i++)
+            {
+                Console.WriteLine(small[i]);
+            }
+
+        }
+    }
+}
